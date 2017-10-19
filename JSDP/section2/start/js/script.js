b880dd6f66a36b4a3c2abbadb38198f04ac359c4
Nicolás Gajardo <n.gajardohernandez@gmail.com>
2017-10-18 22:01:40 -0300

    IIFE 2.7

--- JSDP/section2/start/js/script.js
+++ JSDP/section2/start/js/script.js
@@ -1,12 +1,70 @@
-(function(win, $){
-	$(win.document).ready(function(){
-		$('.advert').click(function(e){
-			var circle = $('<div class="circle"></div>');
-				circle.css('left',e.pageX-25);
-				circle.css('top',e.pageY-25)
-			$('.advert').append(circle);
-		});
+(function(win, $) {
+  var CircleGeneratorSingleton = (function() {
+    var instance;
 
-	});
+    function init() {
+      var _aCircle = [],
+        _stage = $('.advert');
 
-})(window, jQuery);
+      function _position(circle, left, top) {
+        circle.css('left', left);
+        circle.css('top', top);
+      }
+
+      function create(left, top) {
+        var circle = $('<div class="circle"></div>');
+        _position(circle, left, top);
+
+        return circle;
+      }
+
+      function add(circle) {
+        _stage.append(circle);
+        _aCircle.push(circle);
+      }
+
+      function index() {
+        return _aCircle.length;
+      }
+
+      return {
+        index: index,
+        create: create,
+        add: add
+      };
+    }
+
+    return {
+      getInstance: function() {
+        if (!instance) {
+          instance = init();
+        }
+        return instance;
+      }
+    };
+  }());
+
+  $(win.document).ready(function() {
+    $('.advert').click(function(e) {
+      var cg = CircleGeneratorSingleton.getInstance();
+      var circle = cg.create(e.pageX - 25, e.pageY - 25);
+      cg.add(circle);
+    });
+
+    $(document).keypress(function(e) {
+      var cg;
+      var circle;
+
+      if (e.key === 'a') {
+        cg = CircleGeneratorSingleton.getInstance();
+        circle = cg.create(Math.floor(Math.random() * 600),
+          Math.floor(Math.random() * 600));
+      	cg.add(circle);
+      } else if (e.key === 'i') {
+        cg = CircleGeneratorSingleton.getInstance();
+
+        console.log(cg.index());
+      }
+    });
+  });
+}(window, jQuery));
