97e7e45770dd36dc6037d8970a2cac0e328ee82f
Nicolás Gajardo <n.gajardohernandez@gmail.com>
2018-01-31 19:11:19 -0300

    section 4 completed

--- JSDP/section4/start/js/script.js
+++ JSDP/section4/start/js/script.js
@@ -5,18 +5,7 @@
 		}
 	}
 	function Circle(){
-		
 		this.item = $('<div class="circle"></div>');
-		var self = this;
-		this.opacity = 1;
-
-		this.fade=function(){
-			this.opacity *=.5;
-			this.item.fadeTo(.5,this.opacity);
-		}
-		this.item.click(function(){
-			self.fade();
-		});
 	}
 	Circle.prototype.color = function(clr){
 		this.item.css('background', clr);
@@ -31,20 +20,42 @@
 		return this.item;
 	}
 
+	Circle.prototype.getID = function(){
+		return this.id;
+	}
+
+	Circle.prototype.setID = function(id){
+		this.id = id;
+	}
+
 	function Rect(){
 		this.item = $('<div class="rect"></div>');
 	}
 	clone(Circle, Rect);
-
+	
+	function binder(scope, fun){
+		return function(){
+			return fun.apply(scope, arguments);
+		};
+	}
+	
+	function shapeFacade(shp){
+		return {
+			color:  binder(shp, shp.color),
+			move: binder(shp, shp.move),
+			getID: binder(shp, shp.getID)
+		};
+	}
+	
 	function selfDestructDecorator(obj){
 		obj.item.click(function(){
 			obj.kill();
 		});
-		obj.kill = function(){
+		
+		obj.kill = function () {
 			obj.item.remove();
-		};
+		}
 	}
-
 
 	function RedCircleBuilder(){
 		this.item = new Circle();
@@ -57,7 +68,6 @@
 	RedCircleBuilder.prototype.get = function() {
 		return this.item;
 	};
-
 	
 	function BlueCircleBuilder(){
 		this.item = new Circle();
@@ -72,11 +82,13 @@
 				rect.color("yellow");
 				rect.move(40,40);
 				selfDestructDecorator(rect);
+
 		this.item.get().append(rect.get());
 	}; 
 	BlueCircleBuilder.prototype.get = function() {
 		return this.item;
-	};	
+	};
+	
 
 	ShapeFactory = function(){
 			this.types = {};
@@ -90,35 +102,39 @@
 				}
 			}
 	};
-
-	function Stage(id){
+	
+	function StageAdapter(id) {
 		this.index = 0;
 		this.context = $(id);
-		this.SIG = 'stageItem_';
 	}
 
-	Stage.prototype.add = function (item){
-		++this.index;
+	StageAdapter.prototype.SIG = 'stageItem_';
+	StageAdapter.prototype.add = function (item) {
+		++this.index; 
 		item.addClass(this.SIG + this.index);
 		this.context.append(item);
 	};
-
-	Stage.prototype.remove = function(index){
+	StageAdapter.prototype.remove = function (index){
 		this.context.remove('.' + this.SIG + index);
 	}
-
-	function CompositeController(a){
-		this.a = a;
+	
+	function CompositeController(array) {
+		this.array = array;
 	}
-
-	CompositeController.prototype.action = function (act){
+	
+	CompositeController.prototype.action = function (act) {
 		var args = Array.prototype.slice.call(arguments);
-				args.shift();
-		for(var item in this.a){
-			this.a[item][act].apply(this.a[item],args);
+		args.shift();
+		for(var item in this.array){
+			this.array[item][act].apply(this.array[item], args);
 		}
 	};
 
+	function flyWeightFader(item) {
+		if(item.hasClass('circle')){
+			item.fadeTo(.5, item.css('opacity')*.5);
+		}
+	}
 
 	var CircleGeneratorSingleton = (function(){
 		var instance;
@@ -126,38 +142,43 @@
 		function init(){
 			var _aCircle = [],
 					_stage,
-					_sf = new ShapeFactory(),
+					_sf = new ShapeFactory();
 					_cc = new CompositeController(_aCircle);
 
 			function _position(circle, left, top){
 				circle.move(left, top);
 			}
-			function registerShape(name,cls){
+			
+			function registerShape( name, cls){
 				_sf.register(name, cls);
 			}
-			function setStage(stg){
+			
+			function setStage(stg) {
 				_stage = stg;
 			}
 
 			function create(left, top,type){
 				var circle = _sf.create(type);
 				circle.move(left, top);
-				return circle;
-			}
+				circle.setID(_aCircle.length);
+				_aCircle.push(circle);
 
+				return shapeFacade(circle);
+			}
+			
 			function tint(clr){
-				_cc.action('color',clr);
+				_cc.action('color', clr);
 			}
 
-			function move(left, top){
-				_cc.action('move',left, top);
+			function move (left, top){
+				_cc.action('move', left, top);
 			}
 
 			function add(circle){
-				_stage.add(circle.get());
-				_aCircle.push(circle);
+				_stage.add(_aCircle[circle.getID()].get());
+				//_aCircle.push(circle);
 			}
-
+			
 			function index(){
 				return _aCircle.length;
 			}
@@ -166,7 +187,7 @@
 							create:create,
 							add:add,
 							register:registerShape,
-							setStage:setStage,
+							setStage:setStage, 
 							tint:tint,
 							move:move};
 		}
@@ -187,31 +208,33 @@
 		var cg = CircleGeneratorSingleton.getInstance();
 		cg.register('red', RedCircleBuilder);
 		cg.register('blue', BlueCircleBuilder);
-		cg.setStage(new Stage('.advert'));
+		cg.setStage(new StageAdapter('.advert'));
+		
 		$('.advert').click(function(e){
 			var circle = cg.create(e.pageX-25, e.pageY-25,"red");
 
 			cg.add(circle);
-				
+			flyWeightFader($(e.target));
 		});
 
 		$(document).keypress(function(e){
-			if(e.key==='a'){
+			if(e.key==='r'){
 				var circle = cg.create(Math.floor(Math.random()*600),
 															Math.floor(Math.random()*600),
 															"blue");
-				
 				cg.add(circle);
-			}else if(e.key==='t'){
+			} else if(e.key === 't'){
 				cg.tint('black');
-			}else if(e.key==='ArrowRight'){
-				cg.move("+=5px","+=0px");
-			}else if(e.key==='ArrowLeft'){
-				cg.move("-=5px","+=0px");
+			} else if(e.key === 'd')	{
+				cg.move('+=5px', '+=0px');
+			} else if(e.key === 'a'){
+				cg.move('-=5px', '-=0px');
+			} else if(e.key === 's'){
+				cg.move('+=0px', '+=5px');
+			} else if(e.key === 'w'){
+				cg.move('-=0px', '-=5px');
 			}
-			
 		});
-
 	});
 
 })(window, jQuery);
